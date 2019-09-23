1c0194fa4f7bdb89119a7bdca171ce8c1ae91705
SolitudeSF <solitudesf@protonmail.com>
2019-09-23 19:39:58 +0300

    Add docs to `colors` module. Remove redundant template.

--- src/imageman/colors.nim
+++ src/imageman/colors.nim
@@ -30,34 +30,33 @@ template `g=`*(c: var Color, i: untyped) = c[1] = i
 template `b=`*(c: var Color, i: untyped) = c[2] = i
 template `a=`*(c: var ColorA, i: untyped) = c[3] = i
 
-template maxValue*(t: typedesc[Color]): untyped =
-  when T is float32:
-    1.0
-  elif T is uint8:
-    255
-
-template componentType*(t: typedesc[Color]): untyped =
-  when T is ColorRGBFAny:
+template componentType*(t: typedesc[Color]): typedesc =
+  ## Returns component type of a given color type.
+  when t is ColorRGBFAny:
     float32
   else:
     uint8
 
 template maxComponentValue*(t: typedesc[Color]): untyped =
-  when T is ColorRGBFAny:
+  ## Returns maximum component value of a give color type.
+  when t is ColorRGBFAny:
     1.0'f32
   else:
     255'u8
 
 template precise*[T: ColorComponent](t: T): float32 =
+  ## Converts component to float32 if it isn't.
   when T is uint8:
     t.float32
   else:
     t
 
 func toLinear*(c: uint8): float32 =
+  ## Converts 0..255 uint8 to 0..1 float32
   c.float32 / 255
 
 func toUint8*(c: float32): uint8 =
+  ## Converts 0..1 float32 to 0..255 uint8
   uint8(c * 255)
 
 func toRGB*(c: ColorRGBAU): ColorRGBU =
@@ -90,9 +89,11 @@ func toRGBA*(c: ColorRGBAF): ColorRGBAU =
   ColorRGBAU [c.r.toUint8, c.g.toUint8, c.b.toUint8, c.a.toUint8]
 
 func blendColorValue*[T: ColorComponent](a, b: T, t: float32): T {.inline.} =
+  ## Blends two color with ratio.
   T sqrt((1.0 - t) * a.precise * a.precise + t * b.precise * b.precise)
 
 func `+`*[T: Color](a, b: T): T =
+  ## Blends two colors.
   result.r = blendColorValue(a.r, b.r, 0.3)
   result.g = blendColorValue(a.g, b.g, 0.3)
   result.b = blendColorValue(a.b, b.b, 0.3)
@@ -106,9 +107,11 @@ func `$`*(c: ColorRGBU | ColorRGBF): string =
   "(r: " & $c.r & ", g: " & $c.g & ", b: " & $c.b & ")"
 
 func `~=`*(a, b: ColorRGBAF, e = 0.01'f32): bool =
+  ## Compares colors with given accuracy.
   abs(a.r - b.r) < e and abs(a.g - b.g) < e and abs(a.b - b.b) < e
 
 proc rand*[T: Color]: T =
+  ## Returns random color.
   T (when T is ColorRGBU:
     [uint8 rand(255), uint8 rand(255), uint8 rand(255)]
   elif T is ColorRGBAU:
@@ -120,6 +123,7 @@ proc rand*[T: Color]: T =
   )
 
 func rand*[T: Color](r: var Rand): T =
+  ## Returns random color.
   T (when T is ColorRGBU:
     [uint8 r.rand(255), uint8 r.rand(255), uint8 r.rand(255)]
   elif T is ColorRGBAU:
@@ -131,9 +135,11 @@ func rand*[T: Color](r: var Rand): T =
   )
 
 func isGreyscale*(c: Color): bool =
+  ## Checks if color is grayscale (all color components are equal).
   c.r == c.g and c.r == c.b
 
 func interpolate*[T: Color](a, b: T, x: float32, L = 1.0): T =
+  ## Returns linearly interpolated color value.
   result.r = (T.componentType) (a.r.precise + x * (b.r.precise - a.r.precise) / L)
   result.g = (T.componentType) (a.g.precise + x * (b.g.precise - a.g.precise) / L)
   result.b = (T.componentType) (a.b.precise + x * (b.b.precise - a.b.precise) / L)
