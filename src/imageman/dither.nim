448bbd3566a229c7fb77a5503fe305dea02d3389
SolitudeSF <solitudesf@protonmail.com>
2021-01-05 21:18:16 +0200

    Add sugar to make dithering routines more readable

--- src/imageman/dither.nim
+++ src/imageman/dither.nim
@@ -1,4 +1,5 @@
 import ./images, ./colors, ./filters
+import macros
 
 func addError[T: Color](i: var Image[T], x, y: int, factor, r, g, b: float32) {.inline.} =
   if not (x < 0 or y >= i.h or x >= i.w):
@@ -7,87 +8,125 @@ func addError[T: Color](i: var Image[T], x, y: int, factor, r, g, b: float32) {.
     i[idx].g = (T.componentType) (i[idx].g.precise + g * factor)
     i[idx].b = (T.componentType) (i[idx].b.precise + b * factor)
 
+macro addErrors(image: var Image, x, y: int, r, g, b, denominator: float32, coeffs: array): untyped =
+  proc offset(i, o: NimNode): NimNode =
+    result = if o.intVal > 0:
+      nnkInfix.newTree(ident"+", i, o)
+    elif o.intVal < 0:
+      nnkInfix.newTree(ident"-", i, o.intVal.abs.int.newLit)
+    else:
+      i
+
+  result = newStmtList()
+
+  for i in countup(0, coeffs.len - 1, 3):
+    result.add newCall(
+      bindSym"addError", image,
+      offset(x, coeffs[i]),
+      offset(y, coeffs[i + 1]),
+      nnkInfix.newTree(ident"/", coeffs[i + 2], denominator),
+      r, g, b
+    )
+
 func twoDist*(i: var Image, x, y: int, r, g, b: float32) =
-  i.addError(x + 1, y    , 2/4.0, r, g, b)
-  i.addError(x    , y + 1, 1/4.0, r, g, b)
-  i.addError(x + 1, y + 1, 1/4.0, r, g, b)
+  i.addErrors x, y, r, g, b, 4.0, [
+    1, 0, 2,
+    0, 1, 1,
+    1, 1, 1
+  ]
 
 func floydsteinDist*(i: var Image, x, y: int, r, g, b: float32) =
-  i.addError(x + 1, y    , 7/16.0, r, g, b)
-  i.addError(x - 1, y + 1, 3/16.0, r, g, b)
-  i.addError(x    , y + 1, 5/16.0, r, g, b)
-  i.addError(x + 1, y + 1, 1/16.0, r, g, b)
+  i.addErrors x, y, r, g, b, 16.0, [
+    1, 0, 7,
+    -1,1, 3,
+    0, 1, 5,
+    1, 1, 1
+  ]
 
 func atkinsonDist*(i: var Image, x, y: int, r, g, b: float32) =
-  i.addError(x + 1, y    , 1/8.0, r, g, b)
-  i.addError(x + 2, y    , 1/8.0, r, g, b)
-  i.addError(x - 1, y + 1, 1/8.0, r, g, b)
-  i.addError(x    , y + 1, 1/8.0, r, g, b)
-  i.addError(x + 1, y + 1, 1/8.0, r, g, b)
-  i.addError(x    , y + 2, 1/8.0, r, g, b)
+  i.addErrors x, y, r, g, b, 8.0, [
+    1, 0, 1,
+    2, 0, 1,
+    -1,1, 1,
+    0, 1, 1,
+    1, 1, 1,
+    0, 2, 1
+  ]
 
 func burkeDist*(i: var Image, x, y: int, r, g, b: float32) =
-  i.addError(x + 1, y    , 8/42.0, r, g, b)
-  i.addError(x + 2, y    , 4/42.0, r, g, b)
-  i.addError(x - 2, y + 1, 2/42.0, r, g, b)
-  i.addError(x - 1, y + 1, 4/42.0, r, g, b)
-  i.addError(x    , y + 1, 8/42.0, r, g, b)
-  i.addError(x + 1, y + 1, 4/42.0, r, g, b)
-  i.addError(x + 2, y + 1, 2/42.0, r, g, b)
+  i.addErrors x, y, r, g, b, 42.0, [
+    1, 0, 8,
+    2, 0, 4,
+    -2,1, 2,
+    -1,1, 4,
+    0, 1, 8,
+    1, 1, 4,
+    2, 1, 2
+  ]
 
 func sierraDist*(i: var Image, x, y: int, r, g, b: float32) =
-  i.addError(x + 1, y    , 5/32.0, r, g, b)
-  i.addError(x + 2, y    , 3/32.0, r, g, b)
-  i.addError(x - 2, y + 1, 2/32.0, r, g, b)
-  i.addError(x - 1, y + 1, 3/32.0, r, g, b)
-  i.addError(x    , y + 1, 5/32.0, r, g, b)
-  i.addError(x + 1, y + 1, 4/32.0, r, g, b)
-  i.addError(x + 2, y + 1, 2/32.0, r, g, b)
-  i.addError(x - 1, y + 2, 2/32.0, r, g, b)
-  i.addError(x    , y + 2, 3/32.0, r, g, b)
-  i.addError(x + 1, y + 2, 2/32.0, r, g, b)
+  i.addErrors x, y, r, g, b, 32.0, [
+    1, 0, 5,
+    2, 0, 3,
+    -2,1, 2,
+    -1,1, 3,
+    0, 1, 5,
+    1, 1, 4,
+    2, 1, 2,
+    -1,2, 2,
+    0, 2, 3,
+    1, 2, 2
+  ]
 
 func sierra2Dist*(i: var Image, x, y: int, r, g, b: float32) =
-  i.addError(x + 1, y    , 4/16.0, r, g, b)
-  i.addError(x + 2, y    , 3/16.0, r, g, b)
-  i.addError(x - 2, y + 1, 1/16.0, r, g, b)
-  i.addError(x - 1, y + 1, 2/16.0, r, g, b)
-  i.addError(x    , y + 1, 3/16.0, r, g, b)
-  i.addError(x + 1, y + 1, 2/16.0, r, g, b)
-  i.addError(x + 2, y + 1, 1/16.0, r, g, b)
+  i.addErrors x, y, r, g, b, 16.0, [
+    1, 0, 4,
+    2, 0, 3,
+    -2,1, 1,
+    -1,1, 2,
+    0, 1, 3,
+    1, 1, 2,
+    2, 1, 1
+  ]
 
 func sierraLiteDist*(i: var Image, x, y: int, r, g, b: float32) =
-  i.addError(x + 1, y    , 2/4.0, r, g, b)
-  i.addError(x - 1, y + 1, 1/4.0, r, g, b)
-  i.addError(x    , y + 1, 1/4.0, r, g, b)
+  i.addErrors x, y, r, g, b, 4.0, [
+    1, 0, 2,
+    -1,1, 1,
+    0, 1, 1
+  ]
 
 func jarvisDist*(i: var Image, x, y: int, r, g, b: float32) =
-  i.addError(x + 1, y    , 7/48.0, r, g, b)
-  i.addError(x + 2, y    , 5/48.0, r, g, b)
-  i.addError(x - 2, y + 1, 3/48.0, r, g, b)
-  i.addError(x - 1, y + 1, 5/48.0, r, g, b)
-  i.addError(x    , y + 1, 7/48.0, r, g, b)
-  i.addError(x + 1, y + 1, 5/48.0, r, g, b)
-  i.addError(x + 2, y + 1, 3/48.0, r, g, b)
-  i.addError(x - 2, y + 2, 1/48.0, r, g, b)
-  i.addError(x - 1, y + 2, 3/48.0, r, g, b)
-  i.addError(x    , y + 2, 5/48.0, r, g, b)
-  i.addError(x + 1, y + 2, 3/48.0, r, g, b)
-  i.addError(x + 2, y + 2, 1/48.0, r, g, b)
+  i.addErrors x, y, r, g, b, 48.0, [
+    1, 0, 7,
+    2, 0, 5,
+    -2,1, 3,
+    -1,1, 5,
+    0, 1, 7,
+    1, 1, 5,
+    2, 1, 3,
+    -2,2, 1,
+    -1,2, 3,
+    0, 2, 5,
+    1, 2, 3,
+    2, 2, 1
+  ]
 
 func stuckiDist*(i: var Image, x, y: int, r, g, b: float32) =
-  i.addError(x + 1, y    , 8/42.0, r, g, b)
-  i.addError(x + 2, y    , 4/42.0, r, g, b)
-  i.addError(x - 2, y + 1, 2/42.0, r, g, b)
-  i.addError(x - 1, y + 1, 4/42.0, r, g, b)
-  i.addError(x    , y + 1, 8/42.0, r, g, b)
-  i.addError(x + 1, y + 1, 4/42.0, r, g, b)
-  i.addError(x + 2, y + 1, 2/42.0, r, g, b)
-  i.addError(x - 2, y + 2, 1/42.0, r, g, b)
-  i.addError(x - 1, y + 2, 2/42.0, r, g, b)
-  i.addError(x    , y + 2, 4/42.0, r, g, b)
-  i.addError(x + 1, y + 2, 2/42.0, r, g, b)
-  i.addError(x + 2, y + 2, 1/42.0, r, g, b)
+  i.addErrors x, y, r, g, b, 42.0, [
+    1, 0, 8,
+    2, 0, 4,
+    -2,1, 2,
+    -1,1, 4,
+    0, 1, 8,
+    1, 1, 4,
+    2, 1, 2,
+    -2,2, 1,
+    -1,2, 2,
+    0, 2, 4,
+    1, 2, 2,
+    2, 2, 1
+  ]
 
 template dither*[T: ColorRGBAny](i: var Image[T], dist) =
   for y in 0..<i.h:
